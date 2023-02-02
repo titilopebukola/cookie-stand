e4dc68d7d935f19d0a5ea823e474930d9a455b71
titilopebukola <titilopebukola@gmail.com>
2023-02-02 10:44:45 +0000

    Add form validation

--- app.js
+++ app.js
@@ -68,8 +68,8 @@ CookieStore.prototype.render = function () {
   tr.appendChild(td);
 
   // table footer
-  // let tf = document.createElement("tf");
-  // tf.textContent = this.total;
+  // let tfoot = document.createElement("tfoot");
+  // tfoot.textContent = this.sumCookiesSold;
   // tr.appendChild(td);
 
   // get data into the row
@@ -87,6 +87,10 @@ function makeHeaderRow() {
   // table row
   const tr = document.createElement("tr");
   table.appendChild(tr);
+
+  // // table footer
+  // const tfoot = document.querySelectorAll("td");
+  // tfoot.appendChild(td);
 
   // starting cell
   let th = document.createElement("th");
@@ -107,6 +111,7 @@ const dubai = new CookieStore("Dubai", 11, 38, 3.7);
 const paris = new CookieStore("Paris", 20, 38, 2.3);
 const lima = new CookieStore("Lima", 2, 16, 4.6);
 const total = new CookieStore("Total");
+
 seattle.render();
 tokyo.render();
 dubai.render();
@@ -116,24 +121,36 @@ total.render();
 
 // Event Listeners
 
+// Event Listeners
+
 const form = document.getElementById("new-store-form");
 
 form.addEventListener("submit", function (event) {
   event.preventDefault();
+
+  const storeNameInput = event.target.name.value;
+  const minCustInput = event.target.minCust.value;
+  const maxCustInput = event.target.maxCust.value;
+  const avgCookieInput = event.target.avgCookies.value;
 
   console.log(storeNameInput);
   console.log(minCustInput);
   console.log(maxCustInput);
   console.log(avgCookieInput);
 
-  form.requestFullscreen();
+  form.reset();
 
   const newStore = new CookieStore(
     storeNameInput,
+    minCustInput,
     maxCustInput,
     avgCookieInput
   );
+
+  newStore.render();
 });
+
+// form validatiom
 
 // const seattle = {
 //   name: "Seattle",
